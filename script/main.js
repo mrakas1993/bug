ca9b884e665bf08657d5fa11860970fe78875f66
mrakas1993 <mrakas1993@gmail.com>
2024-05-19 18:13:17 +0300

    add project

---
+++ script/main.js
@@ -0,0 +1,29 @@
+//header-slider
+const slides = document.querySelectorAll('.slide-item'),
+        prev = document.querySelector('.slider-prev'),
+        next = document.querySelector('.slider-next');
+
+    let slideIndex = 1;
+
+    showSlides();
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+    }
+
+    function plusSlide(n) {
+        showSlides(slideIndex += n); 
+    }
+    next.addEventListener('click', () => {
+        plusSlide(1);
+    })
+    prev.addEventListener('click', () => {
+        plusSlide(-1);
+    })
